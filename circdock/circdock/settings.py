0f0385c9bcf8bc8a087562c89f8fcb5033dcd97f
Michael Lennon <mslennon@thoughtworks.com>
2018-03-17 00:23:09 -0500

    use postgres user

--- circdock/circdock/settings.py
+++ circdock/circdock/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'mslennon',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'postgres-host',
         'PORT': '5432',
